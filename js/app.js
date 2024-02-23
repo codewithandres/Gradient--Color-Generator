54d1069fa38fccd5aed4b13fa6b0b9262e3f0ab8
Dizze web <axiozweb202@gmail.com>
2024-02-23 17:32:49 -0500

    Generador de Degradados HTML CSS JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const colorInput = document.querySelectorAll('.color input');
-
+const gradientBox = document.querySelector('.gradient-box');
 const generateGradient = () => {
     const gradient = `linear-gradient(to top left , ${colorInput[0].value}, ${colorInput[1].value})`;
-    console.log(gradient);
+    gradientBox.style.background = gradient;
 }
 
 colorInput.forEach(input => {
