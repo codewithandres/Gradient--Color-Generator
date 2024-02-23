d5e6694411790c362cfaf5df3f190f37c6df81e0
Dizze web <axiozweb202@gmail.com>
2024-02-23 18:13:18 -0500

    final del proyecto

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
 const colorInput = document.querySelectorAll('.color input');
 const gradientBox = document.querySelector('.gradient-box');
-const generateGradient = () => {
-    const gradient = `linear-gradient(to top left , ${colorInput[0].value}, ${colorInput[1].value})`;
+const selectedMenu = document.querySelector('.selectet-box select');
+const textarea = document.querySelector('textarea');
+const refresh = document.querySelector('.refresh');
+const copyBtn = document.querySelector('.copy');
+
+const getRamdonColor = () => {
+    const ramdonHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${ramdonHex}`;
+}
+
+const generateGradient = (isRamdom) => {
+
+    if (isRamdom) {
+        colorInput[0].value = getRamdonColor();
+        colorInput[1].value = getRamdonColor();
+    }
+    const gradient = `linear-gradient(${selectedMenu.value}, ${colorInput[0].value}, ${colorInput[1].value})`;
+
     gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient}`;
+}
+const copyCode = () => {
+    navigator.clipboard.writeText(textarea.value)
+    copyBtn.innerText = 'codigo copiado';
+    setTimeout(() => {
+        copyBtn.innerText = 'copiar Codigo';
+    }, 1600);
 }
 
 colorInput.forEach(input => {
-    input.addEventListener('input', generateGradient);
+    input.addEventListener('input', () => generateGradient(false));
 });
+
+
+selectedMenu.addEventListener('change', () => generateGradient(false));
+refresh.addEventListener('click', () => generateGradient(true))
+copyBtn.addEventListener('click', copyCode);
